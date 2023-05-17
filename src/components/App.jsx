2eb81e6d763059442aeca928d451e94f985753c1
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-05-17 09:32:02 +0200

    React-hw-6-repozitori

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <p>goit-react-hw-06-phonebook</p>
     </div>
   );
 };
